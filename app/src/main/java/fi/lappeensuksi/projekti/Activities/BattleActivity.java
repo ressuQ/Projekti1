3547fec60d2daf369947ea13ecc6d0bcfd129ad0
ressuQ <ress.out@gmail.com>
2023-04-27 14:23:40 +0300

    -

--- app/src/main/java/fi/lappeensuksi/projekti/Activities/BattleActivity.java
+++ app/src/main/java/fi/lappeensuksi/projekti/Activities/BattleActivity.java
@@ -52,8 +52,8 @@ public class BattleActivity extends AppCompatActivity {
 
                 battlestring=battlestring+(lutemon2.getColor() + "(" + lutemon2.getName() + ") gets killed.\nThe battle is over.");
                 exit = true;
-                storage.getBattleLutemons().remove(1);
-                storage.getAllLutemons().remove(1);
+                storage.getBattleLutemons().remove(lutemon1);
+                storage.getAllLutemons().remove(lutemon1);
                 break;
             }
             random=(int)Math.random()*3;
@@ -68,8 +68,8 @@ public class BattleActivity extends AppCompatActivity {
 
                 battlestring=battlestring+(lutemon1.getColor() + "(" + lutemon1.getName() + ") gets killed.\nThe battle is over.");
                 exit = true;
-                storage.getBattleLutemons().remove(0);
-                storage.getAllLutemons().remove(0);
+                storage.getBattleLutemons().remove(lutemon2);
+                storage.getAllLutemons().remove(lutemon2);
             }
         }
         System.out.println(battlestring);
